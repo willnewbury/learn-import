4b928479685fef2d4bc748ecba8a848e81cc2837
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-08-16 09:45:28 -0500

    ISSITES-5607 Clean up imports

--- import_learn.py
+++ import_learn.py
@@ -1,12 +1,14 @@
-import requests
-import json
-import os
-import config
-import oauth_token
-import import_file
+import configuration
+import get_articles
 import import_article
+import import_file
+import oauth_token
+import os
+import requests
 
-config = config.getConfig()
+LEARN_ARTICLE_JSON_EXTENSION = ".fjson"
+
+config = configuration.getConfig()
 
 print ("Using host " + config['OAUTH_HOST'])
 
@@ -23,12 +25,15 @@ def importImages():
 				print("Importing... " + filename)
 				import_file.importFile(filename, "commerce_" + f, config,authorization)
 
-importImages()
+#importImages()
+#articles = get_articles.getArticles(config, authorization)
+#print(json.dumps(articles, indent=4))
+
 
 def importArticles():
 	for root,d_names,f_names in os.walk(config['SPHINX_OUTPUT_DIRECTORY']):
 		for f in f_names:
-			if f.endswith(".fjson"):
+			if f.endswith(LEARN_ARTICLE_JSON_EXTENSION):
 				filename = os.path.join(root, f)
 				print("Importing... " + filename)
 				import_article.importArticle(filename, config, authorization)
