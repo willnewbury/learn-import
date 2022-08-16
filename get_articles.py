4c03a44916aaf0fe70b92df47007742952cf2eaa
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-08-16 09:17:55 -0500

    ISSITES-5607 Add get_articles function

---
+++ get_articles.py
@@ -0,0 +1,16 @@
+import requests
+
+def getArticles(config, authorization):
+	headers = {
+		"Accept": "application/json",
+		"Authorization": authorization,
+        "Content-Type": "application/json"
+	}
+
+	session = requests.Session()
+
+	get_uri = f"{config['OAUTH_HOST']}/o/headless-delivery/v1.0/sites/{config['SITE_ID']}/structured-contents?fields=id,friendlyUrlPath"
+
+	res = session.get(get_uri, headers=headers)
+
+	return res.json()
